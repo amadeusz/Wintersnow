a493776ac90ebe093f88ccf65186830d7cff8178
amadeusz <maciek@eldesign.eu>
2010-09-11 01:55:35 +0200

    Walidacja+++ Kosmetyka++

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,43 @@
+function setSelectionRange(input, selectionStart, selectionEnd) {
+	if (input.setSelectionRange) {
+		input.focus();
+		input.setSelectionRange(selectionStart, selectionEnd);
+	}
+	else if (input.createTextRange) {
+		var range = input.createTextRange();
+		range.collapse(true);
+		range.moveEnd('character', selectionEnd);
+		range.moveStart('character', selectionStart);
+		range.select();
+	}
+}
+
+function setCaretToPos (input, pos) {
+	setSelectionRange(input, pos, pos);
+}
+
 $(document).ready( function() {
-	$("#genotypes input[type=text]:last").focus();
+
+	// Focusowanie formularzy
 
 	$("#addresses_new input[type=text]:first").focus();
-	$("#addresses_create input[type=text]:first").focus();
-	$("#addresses_update input[type=text]:first").focus();
-	$("#addresses_edit input[type=text]:first").focus();
+	$("#genotypes input[type=text]:last").focus();
+	$(".field_with_errors input[type=text]:first").focus(function() { setCaretToPos(this, this.value.length); });
+	$(".field_with_errors input[type=text]:first").focus();
+
+	// Tooltipy w adresach
 
 	$("#addresses .komunikat_id").hover(	
 		function(){
-			$(this).css({backgroundColor:"#FFC0CB"});
+			$(this).css({ backgroundColor: "#FFC0CB" });
 			$(this).children("div").show();
 		},
 		function(){
-			$(this).css({backgroundColor:"transparent"});
+			$(this).css({ backgroundColor: "transparent" });
 			$(this).children("div").fadeOut(300);
 		}
 	);
+	
+	
 });
 
