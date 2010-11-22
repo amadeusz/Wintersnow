3e27193e942db81bbd85188ead13c6c041704889
amadeusz <maciek@eldesign.eu>
2010-11-22 22:24:08 +0100

    User.messages, sortowanie wiadomosci

--- app/helpers/rss_helper.rb
+++ app/helpers/rss_helper.rb
@@ -1,17 +1,5 @@
 module RssHelper
 	def news(user)
-	
-		news = []
-		if user.created_at != nil then
-			User.find(user).addresses.each { |adres|
-				news += adres.messages.where("data >= ?", user.created_at).order("data DESC")
-			}
-		else
-			User.find(user).addresses.each { |adres|
-				news += adres.messages.order("data DESC")
-			}	
-		end
-		news
-		
+		user.messages
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,13 @@ class User < ActiveRecord::Base
 	
 	before_create :generate_rss_pass
 	
+	def messages
+		Message.find_by_sql("SELECT m.*
+		FROM sites s, addresses a, messages m
+		WHERE s.user_id = #{id} AND s.address_id = a.id AND m.address_id = a.id AND m.data >= \"#{created_at}\"
+		ORDER BY m.data DESC")
+	end
+	
 	private
 		def generate_rss_pass
 			self.rss_pass = (Digest::SHA2.new() << (self.klucz + rand(100).to_s)).to_s[1..20]
