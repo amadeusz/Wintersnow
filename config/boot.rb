c48c8c67ab99418405fcaac4ccdb98d5de3837e2
amadeusz <maciek@eldesign.eu>
2010-12-05 01:35:13 +0100

    appload deploy fixes

--- config/boot.rb
+++ config/boot.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 gemfile = File.expand_path('../../Gemfile', __FILE__)
 begin
   ENV['BUNDLE_GEMFILE'] = gemfile
+#  ENV['RAILS_ENV'] = "development"
   require 'bundler'
   Bundler.setup
 rescue Bundler::GemNotFound => e
