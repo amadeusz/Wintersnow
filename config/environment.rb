c4828f80750f4f4f39c25f69f8c7a83b6a93164c
LittleD <pisz.na@gmail.com>
2011-03-03 14:24:41 +0100

    Data na głównej

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ require File.expand_path('../../lib/core_extensions', __FILE__)
 
 # Initialize the rails application
 Koliber::Application.initialize!
+PL_DAYS = [nil,'poniedziałek','wtorek','środa','czwartek','piatek','sobota','niedziela']
+PL_MONTHS = [nil,'stycznia','lutego','marca','kwietnia','maja','czerwca','lipca','sierpnia','września','października','listopada','grudnia']
