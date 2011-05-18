575d71d1baff9a677c61e8bd80e1da94ab199f83
LittleD <pisz.na@gmail.com>
2011-05-18 11:47:51 +0200

    Kalendarz sprawdzony, znajduje się w ApplicationHelpers(żeby można było dynamicznie zmieniać eventy, bez restartu koliberka

--- lib/calendar_const.rb
+++
@@ -1,6 +0,0 @@
-PL_DAYS = [nil,'poniedziałek','wtorek','środa','czwartek','piatek','sobota','niedziela']
-PL_MONTHS = [nil,'stycznia','lutego','marca','kwietnia','maja','czerwca','lipca','sierpnia','września','października','listopada','grudnia']
-POLIBUDA = {
-	"20-04-2011" => "zajęcia jak w piątek parzysty"
-	"16-05-2011" => "zajęcia jak w piątek nieparzysty" 
-	}
